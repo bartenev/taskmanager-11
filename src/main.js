52889f4a20011742069ed6a39cdba3db5ad9f23b
Илья <bartenev.ilya@gmail.com>
2021-08-24 15:18:08 +0300

    Создает и подключает модель данных

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import SiteMenuComponent from './components/site-menu.js';
 import FilterComponent from './components/filter.js';
 import BoardComponent from './components/board.js';
 import BoardController from "./controllers/board";
+import TasksModel from "./models/tasks.js";
 import {generateFilters} from "./mock/filter.js";
 import {generateTasks} from "./mock/task.js";
 import {render} from "./utils/render.js";
@@ -13,6 +14,8 @@ const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const filters = generateFilters();
 const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
 
 render(siteHeaderElement, new SiteMenuComponent());
 render(siteMainElement, new FilterComponent(filters));
@@ -20,5 +23,5 @@ render(siteMainElement, new FilterComponent(filters));
 const boardComponent = new BoardComponent();
 render(siteMainElement, boardComponent);
 
-const boardController = new BoardController(boardComponent);
+const boardController = new BoardController(boardComponent, tasksModel);
 boardController.render(tasks);
