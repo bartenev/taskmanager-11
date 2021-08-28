4ec7ae378eb8f54ddc59f1c7707f89c43d92cf58
Илья <bartenev.ilya@gmail.com>
2021-08-28 10:57:33 +0300

    Подключит библиотеку chart.js и настроик показ статистики

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -58,6 +58,14 @@ export default class BoardController {
     this._tasksModel.setFilterChangeHandler(this._onFilterChange);
   }
 
+  hide() {
+    this._container.hide();
+  }
+
+  show() {
+    this._container.show();
+  }
+
   render() {
     const tasks = this._tasksModel.getTasks();
     const container = this._container.getElement();
