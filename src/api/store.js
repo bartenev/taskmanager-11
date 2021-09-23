b981e0d97ca356f9bd174d630e84e232695577dc
Илья <bartenev.ilya@gmail.com>
2021-09-23 14:17:44 +0300

    Добавит реализацую методов getItems и setItem в store

--- src/api/store.js
+++ src/api/store.js
@@ -1,13 +1,28 @@
 export default class Store {
-  constructor(store) {
-    this._store = store;
+  constructor(key, storage) {
+    this._storeKey = key;
+    this._storage = storage;
   }
 
   getItems() {
-    return {};
+    try {
+      return JSON.parse(this._storage.getItem(this._storeKey)) || {};
+    } catch (err) {
+      return {};
+    }
   }
 
   setItem(key, value) {
+    const store = this.getItems();
+
+    this._storage.setItem(
+        this._storeKey,
+        JSON.stringify(
+            Object.assign({}, store, {
+              [key]: value
+            })
+        )
+    );
 
   }
 
